3e5de0bbd837862ae097ef7bbdda3f84e235ac9e
ayushbasak <ayushbasak0210@gmail.com>
2020-10-18 23:56:50 +0530

    SWITCH TO POSTGRES
    Apparently, I lost my root access to MySQL by literally changing the
    password and forgetting it, and I don't want to go into the hassle of
    changing it. Downloading the JDBC driver for Postgres was easier.🤦‍♂️

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.sql.*;
 public class Main{
 	public static void main(String [] args) {
 		try {
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/IITJamdoli","root","*Ayush0210*");
+			Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/bank","postgres","ayush0210");
 			Statement st = con.createStatement();
 			ResultSet Rs = st.executeQuery("SELECT * FROM person");
 			
