1213712f9e410acd627116ae9cc63ff1bf8389f8
ayushbasak <ayushbasak0210@gmail.com>
2020-10-28 14:24:47 +0530

    CREATE GUI INTERFACE USING SWING
    Switch from JavaFX to Swing for GUI.
    Implement a very basic interface for
    SignUp layout.

--- src/HelperClasses/SignUp.java
+++ src/HelperClasses/SignUp.java
@@ -1,6 +1,11 @@
 package HelperClasses;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 import java.util.Scanner;
 public class SignUp {
+	
+	//Instance Variables to store consumer details
 	int ConsumerID;
 	String name;
 	String DOB;
@@ -9,6 +14,8 @@ public class SignUp {
 	int pincode;
 	int totalAccounts;
 	
+	
+	//constructors
 	SignUp(){
 		this.ConsumerID = generateConsumerID();
 		this.name = "NULL";
@@ -25,16 +32,24 @@ public class SignUp {
 		this.gender = gender;
 		this.address = address;
 		this.pincode = pincode;
-		this.totalAccounts = totalAccounts;
+		this.totalAccounts = totalAccounts;	
 	}
 	
 	public static String createSQLStatement(){
+		SignUp object = new SignUp();
+		object.createGUI();
+		
 		Scanner in = new Scanner(System.in);
-		System.out.println("Enter Your Details");
+		System.out.println("Enter Your Details:\n");
+		System.out.print("NAME (20 char limit):\t");
 		String name = in.nextLine();
+		System.out.print("DOB (DD-MM-YYYY):\t");
 		String DOB = in.nextLine();
+		System.out.print("GENDER (M/F/O):\t");
 		char gender = in.nextLine().charAt(0);
+		System.out.print("ADDRESS (100 char limit):\t");
 		String address = in.nextLine();
+		System.out.print("PINCODE (7 digit):\t");
 		int pincode = in.nextInt();
 		
 		SignUp obj = new SignUp(name,DOB,gender,address,pincode,0);
@@ -47,7 +62,80 @@ public class SignUp {
 		in.close();
 		return SQLStatement;
 	}
+	void createGUI() {
+		//GUI (Swing)
+		JFrame signupFrame = new JFrame("Hello");
+		signupFrame.setSize(500,600);
+		signupFrame.setLayout(null);
+		signupFrame.setVisible(true);
+		signupFrame.getContentPane().setBackground(new Color(30,200,200));
+		
+		JLabel header = new JLabel("Bank Of IIT-Jamdoli");
+		header.setBounds(120,40,200,20);
+		signupFrame.add(header);
+		
+		//Name Field
+		JLabel nameLabel = new JLabel("Name (Max 20 Characters)");
+		nameLabel.setBounds(20,100,200,20);
+		signupFrame.add(nameLabel);
 	
+		JTextField nameField = new JTextField();
+		nameField.setBounds(230,100,150,20);
+		signupFrame.add(nameField);
+		
+		//DOB Field
+		JLabel DOBLabel = new JLabel("DOB (dd-mm-yyyy)");
+		DOBLabel.setBounds(20,130,200,20);
+		signupFrame.add(DOBLabel);
+		
+		JTextField DOBField = new JTextField();
+		DOBField.setBounds(230,130,150,20);
+		signupFrame.add(DOBField);
+		
+		
+		//Gender Field
+		JLabel GenderLabel = new JLabel("Gender (M/F/O)");
+		GenderLabel.setBounds(20,160,200,20);
+		signupFrame.add(GenderLabel);
+		
+		JTextField GenderField = new JTextField();
+		GenderField.setBounds(230,160,150,20);
+		signupFrame.add(GenderField);
+		
+		//Address Field
+		JLabel AddressLabel = new JLabel("Address (Max 100 characters)");
+		AddressLabel.setBounds(20,190,200,20);
+		signupFrame.add(AddressLabel);
+		
+		JTextField AddressField = new JTextField();
+		AddressField.setBounds(230,190,150,20);
+		signupFrame.add(AddressField);
+		
+		//Pincode Field
+		JLabel pincodeLabel = new JLabel("Add Pincode");
+		pincodeLabel.setBounds(20,190,200,20);
+		signupFrame.add(pincodeLabel);
+		
+		JTextField pincodeField = new JTextField();
+		pincodeField.setBounds(230,190,150,20);
+		signupFrame.add(pincodeField);
+		
+		//Button Field
+		JButton createConsumer = new JButton("Sign Up");
+		createConsumer.setBounds(20,240,400,40);
+		signupFrame.add(createConsumer);
+		
+		//DisplayField
+		JLabel display = new JLabel("Display");
+		display.setBounds(20,290,400,40);
+		signupFrame.add(display);
+		
+		createConsumer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				display.setText("LOL");
+			}
+		});
+	}
 	int generateConsumerID() {
 		return (int)(Math.random()*1000000);
 	}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.sql.*;
-
 import HelperClasses.SignUp;
 public class Main{
 	public static void main(String [] args) {
